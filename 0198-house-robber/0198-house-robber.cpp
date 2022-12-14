5beac33677498e126568292fb57656dd6049e7bd
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2022-12-14 22:46:50 +0530

    Time: 3 ms (56.21%), Space: 7.8 MB (25.79%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(int i,vector<int>& nums,int n,vector<int>& dp){
+        if(i>=n){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int take=nums[i]+solve(i+2,nums,n,dp);
+        int not_take=solve(i+1,nums,n,dp);
+        
+       return dp[i]=max(take,not_take);
+        
+        
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return solve(0,nums,n,dp);
+        
+    }
+};
