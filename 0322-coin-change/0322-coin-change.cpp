ddc302d195ea54c8ea97678512fad6e4839ad0ce
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2022-12-27 14:54:55 +0530

    Time: 155 ms (52.15%), Space: 50.1 MB (10.46%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int solve(int i,int amount,vector<int>& coins,vector<vector<int>>& dp){
+        if(i>=coins.size()){
+            return 100000;
+        }
+        if(amount==0){
+            return 0;
+        }
+        if(dp[i][amount] != -1){
+            return dp[i][amount];
+        }
+        int take = 100000;
+        if(coins[i]<=amount){
+             take = 1+solve(i,amount-coins[i],coins,dp);
+        }
+       
+        int not_take=solve(i+1,amount,coins,dp);
+        return dp[i][amount]=min(take,not_take);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        int ans= solve(0,amount,coins,dp);
+        if(ans==100000){
+            return -1;
+        }
+        return ans;
+    }
+};
