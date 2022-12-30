18f5a038dd3b37bec14dac7dd98384d531e4f497
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2022-12-30 22:58:11 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.96%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-         if(dividend == INT_MIN && divisor == 1)
+      if(dividend == INT_MIN && divisor == 1)
             return INT_MIN;
 		// -2147483648/-1 = 2147483647 
 		// [Since after -ve sign cancels 2147483648/1 = 2147483648 remains, which is 1 greater than INT_MAX(2147483647)]
