c7637e3c90e796ad4ce9f5a79625833fd525ff1c
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-01-03 14:53:23 +0530

    Time: 207 ms (10.32%), Space: 35.2 MB (5.58%) - LeetHub

--- 0817-linked-list-components/0817-linked-list-components.cpp
+++ 0817-linked-list-components/0817-linked-list-components.cpp
@@ -10,44 +10,61 @@
  */
 class Solution {
 public:
-  
-    int numComponents(ListNode* head, vector<int>& nums) {
-     std::unordered_map<int, std::vector<int>> adj_list;
-  ListNode* curr = head;
-  while (curr != nullptr) {
-    if (curr->next != nullptr) {
-      adj_list[curr->val].push_back(curr->next->val);
-      adj_list[curr->next->val].push_back(curr->val);
-    }
-    curr = curr->next;
+  void DFS(int src,unordered_map<int,vector<int>>& adj_list,unordered_map<int,bool>& visited){
+      if(visited.find(src)==visited.end()){
+          return;
+      }
+      visited[src]=false;
+      for(auto it:adj_list[src]){
+          if(visited[it]){
+              DFS(it,adj_list,visited);
+          }
+      }
   }
+    int numComponents(ListNode* head, vector<int>& nums) {
+        //vector<vector<int>>adj_list;
+        unordered_map<int, vector<int>> adj_list;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            if (curr->next != nullptr) {
+            adj_list[curr->val].push_back(curr->next->val);
+            adj_list[curr->next->val].push_back(curr->val);
+             }
+        curr = curr->next;
+        }
 
   // Mark all nodes in nums as visited
-  std::unordered_map<int, bool> visited;
+  unordered_map<int, bool> visited;
   for (int num : nums) {
     visited[num] = true;
   }
 
   // Perform a DFS on each node in nums to count the number of connected components
   int count = 0;
-  for (int num : nums) {
-    if (visited[num]) {
-      count++;
-      std::stack<int> stack;
-      stack.push(num);
-      visited[num] = false;
-      while (!stack.empty()) {
-        int curr = stack.top();
-        stack.pop();
-        for (int neighbor : adj_list[curr]) {
-          if (visited[neighbor]) {
-            stack.push(neighbor);
-            visited[neighbor] = false;
-          }
+  // for (int num : nums) {
+  //   if (visited[num]) {
+  //     count++;
+  //     stack<int> stack;
+  //     stack.push(num);
+  //     visited[num] = false;
+  //     while (!stack.empty()) {
+  //       int curr = stack.top();
+  //       stack.pop();
+  //       for (int neighbor : adj_list[curr]) {
+  //         if (visited[neighbor]) {
+  //           stack.push(neighbor);
+  //           visited[neighbor] = false;
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+        for(auto num:nums){
+            if(visited[num]){
+                count++;
+                DFS(num,adj_list,visited);
+            }
         }
-      }
-    }
-  }
 
   return count;
     }
